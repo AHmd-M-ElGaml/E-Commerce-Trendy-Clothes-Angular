1c493b5d7578f3fd8d9d1039c5ae06e14294579f
AHmd El-Gaml <96872763+AHmd-M-ElGaml@users.noreply.github.com>
2022-10-19 12:09:20 +0200

    fixed photos

--- src/app/modules/contact/components/form-details/form-details.component.ts
+++ src/app/modules/contact/components/form-details/form-details.component.ts
@@ -13,15 +13,18 @@ export class FormDetailsComponent implements OnInit {
   }
   persons: any[] = [
     {
-      img: '../../../../../assets/img/people/1.png',
+      // img: '../../../../../assets/img/people/1.png',
+      img: 'https://i.ibb.co/wrNxgKn/1.png',
       name: 'John Doe',
     },
     {
-      img: '../../../../../assets/img/people/2.png',
+      // img: '../../../../../assets/img/people/2.png',
+      img: 'https://i.ibb.co/fnKKsJ9/2.png',
       name: 'William Smith',
     },
     {
-      img: '../../../../../assets/img/people/3.png',
+      // img: '../../../../../assets/img/people/3.png',
+      img: 'https://i.ibb.co/4RQFwTZ/3.png',
       name: 'Emma Stone',
     },
   ]
--- src/app/shared/components/feature/feature.component.ts
+++ src/app/shared/components/feature/feature.component.ts
@@ -12,27 +12,33 @@ export class FeatureComponent  {
   features: any[] = [
     {
       title: 'Free Shipping',
-      img: '../../../../assets/img/features/f1.png',
+      img: 'https://i.ibb.co/Pj0YR9v/f1.png',
+      // img: '../../../../assets/img/features/f1.png',
     },
     {
       title: 'Online Order',
-      img: '../../../../assets/img/features/f2.png',
+      img: 'https://i.ibb.co/zGNTmVB/f2.png',
+      // img: '../../../../assets/img/features/f2.png',
     },
     {
       title: 'Save Money',
-      img: '../../../../assets/img/features/f3.png',
+      img: 'https://i.ibb.co/9wZPK1d/f3.png',
+      // img: '../../../../assets/img/features/f3.png',
     },
     {
       title: 'Promotions',
-      img: '../../../../assets/img/features/f4.png',
+      img: 'https://i.ibb.co/qNghtGq/f4.png',
+      // img: '../../../../assets/img/features/f4.png',
     },
     {
       title: 'Happy Sell',
-      img: '../../../../assets/img/features/f5.png',
+      img: 'https://i.ibb.co/MGd16D6/f5.png',
+      // img: '../../../../assets/img/features/f5.png',
     },
     {
       title: 'F24/7 Support',
-      img: '../../../../assets/img/features/f6.png',
+      img: 'https://i.ibb.co/RcDLfGB/f6.png',
+      // img: '../../../../assets/img/features/f6.png',
     },
   ]
 
